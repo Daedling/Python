9cadb1745b24efef75e0225ea1785986c212f051
Daedling <31654722+Daedling@users.noreply.github.com>
2017-11-14 06:23:53 +0300

    Add files via upload

---
+++ hw3-14.11.17/Add./Вариант 1.py
@@ -0,0 +1,6 @@
+a = str(input("Введите символы: "))
+b = len(a)
+while 0 != b:
+    print(a[0:b])
+    b-=1
+    
---
+++ hw3-14.11.17/Add./Вариант 5.py
@@ -0,0 +1,7 @@
+a = str(input("Введите символы: "))
+b = len(a)
+while b != 0:
+    b-=1
+    print(a[b:len(a)])
+
+    
