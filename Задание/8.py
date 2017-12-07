9cccb4d2b87e44e57f4945b5b49db7b8fd857672
Daedling <31654722+Daedling@users.noreply.github.com>
2017-12-07 11:45:04 +0300

    Add files via upload

--- Задание/8.py
+++ Задание/8.py
@@ -1,10 +1,12 @@
 f=0
+res=''
 with open('freq.txt', encoding='utf-8') as k:
     z=k.readlines()
     for y in z:
         y=y.replace('\n','')
         w=y.split(' | ')
         if 'част' in w[1].split():
-            print(y)
-            f+=float(w[2])      
+            res+=w[0]+', '
+            f+=float(w[2])
+print(res[:(len(res)-2)])
 print(f)
