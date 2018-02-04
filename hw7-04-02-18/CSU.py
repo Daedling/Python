3a1adc264177855bd9d73dd7d0d32b921a752bd9
Daedling <31654722+Daedling@users.noreply.github.com>
2018-02-04 22:07:36 +0300

    Update CSU.py

--- hw7-04-02-18/CSU.py
+++ hw7-04-02-18/CSU.py
@@ -1,51 +1,50 @@
 import re
 def op():
-    z=input("Название файла, без расширения: ")
-    misc=['\'','\"','!','?',')','(',':',';','.',',']
+    z = input("Название файла, без расширения: ")
+    misc=['\'', '\"', '!', '?', ')', '(', ':', ';', '.', ',']
     res=''
-    with open (z+'.txt', 'r',encoding='utf-8') as k:
-        a=k.read()
+    with open (z+'.txt', 'r', encoding='utf-8') as k:
+        a = k.read()
         for b in a:
             if b not in misc:
-                res+=b
-        res=res.split()        
+                res += b
+        res = res.split()        
         for ct, c in enumerate(res):
-            res[ct]=c.capitalize()      
-        a=res
-        return(a)
+            res[ct] = c.capitalize()      
+        a = res
+        return (a)
 def fun(lst):
-
-    res=[]
-    pat=re.compile('un.*', re.IGNORECASE)
+    res = []
+    pat = re.compile('un.*', re.IGNORECASE)
     for a in lst:
-        if re.match(pat,a)!=None:
+        if re.match(pat,a) != None:
             res.append(a)
     return res
 def checknum():
-    flag=0
+    flag = 0
     num=['1','2','3','4','5','6','7','8','9','0']
-    a=''
+    a = ''
     while flag == 0:
-        a=input("Введите цифру или End для завершения: ")
-        if a !='':
+        a = input("Введите цифру или End для завершения: ")
+        if a != '':
             flag = 1
             for b in a:
-                if b not in num and a!='End':
-                    flag=0
+                if b not in num and a != 'End':
+                    flag = 0
                     break
     return a
 def check(lst,num):
-    total=len(lst)
-    req=0
+    total = len(lst)
+    req = 0
     while num != 'End':
-        num=int(num)
+        num = int(num)
         if total != 0:
             for a in lst:
                 if len(a)>num:
-                    req+=1
+                    req += 1
             print((str(req/total*100)+'%'))
             num = checknum()
-            req=0
+            req = 0
         else:
             return print(('no match'))
     return print('Конец')
