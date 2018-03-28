92512d6a29bd3f62f9f9f2ff7b858be49938d0d6
Daedling <31654722+Daedling@users.noreply.github.com>
2018-03-28 23:12:19 +0300

    Delete fluttering_arrhythmia.py

--- hw10-2-04-18/fluttering_arrhythmia.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('a.html', 'r', encoding ='utf-8') as k:
-    pat=re.compile('iso639-3/documentation\.asp\?id=....')
-    for a in k:
-        m=re.search(pat,a)
-        if m != None:
-            z=m.group()     
-with open('result.txt','w', encoding='utf-8') as k:
-    k.write(z[-4:-1])
