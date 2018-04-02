57d646fbe82dbf64bbe4a9327ccd175fe264b4cb
Daedling <31654722+Daedling@users.noreply.github.com>
2018-04-02 15:22:58 +0300

    Update fluttering_arrhythmia.py

--- hw10-02-04-18/fluttering_arrhythmia.py
+++ hw10-02-04-18/fluttering_arrhythmia.py
@@ -1,9 +1,13 @@
 import re
-with open('Bjargtangar.html', 'r', encoding ='utf-8') as k:
-    pat=re.compile('iso639-3/documentation\.asp\?id=....')
-    for a in k:
-        m=re.search(pat,a)
-        if m != None:
-            z=m.group()     
-with open('result.txt','w', encoding='utf-8') as k:
-    k.write(z[-4:-1])
+def main():
+    s=input('Введите имя фаила: ')
+    with open(s+'.html', 'r', encoding ='utf-8') as k:
+        pat=re.compile('iso639-3/documentation\.asp\?id=....')
+        for a in k:
+            m=re.search(pat,a)
+            if m != None:
+                z=m.group()     
+    with open('result.txt','w', encoding='utf-8') as k:
+        k.write(z[-4:-1])
+if __name__  ==  '__main__':
+    main()         
